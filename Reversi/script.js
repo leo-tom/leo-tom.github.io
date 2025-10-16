91885192de303d3a5810fc567487930fca29e5e7
Leo <ttnistomura@gmail.com>
2025-10-16 23:25:32 +0900

    Fix piece count check for currentBoard

--- Reversi/script.js
+++ Reversi/script.js
@@ -541,7 +541,7 @@ function frameUpdate(){
                     currentBoard.setList(Brain.getList(currentBoard, AI_CHAR), AI_CHAR, true);
                     return;
                 }
-                if(board.countPieces('*') + board.countPieces('o') > 5){
+                if(currentBoard.countPieces('*') + currentBoard.countPieces('o') > 5){
                     if(score >= 0.5){
                         insertFace("laugh");
                         playVoice(VOICE_ID_PUT_LAUGHING);
